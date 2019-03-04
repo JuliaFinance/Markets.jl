86c83e04ef7dfe27ae6bb1e0bc7215f2690a293e
Eric Forgy <Eric.Forgy@coherentasia.com>
2019-03-04 22:55:37 +0800

    Added FX module

--- src/Markets.jl
+++ src/Markets.jl
@@ -1,14 +1,36 @@
 module Markets
 
 using Reexport
-@reexport using Currencies
-export Market
+@reexport using Positions
+export FX
 
-struct Market{C<:Currency}
-    name::String
-    # Yield curves
-    # Equity prices
-    # Etc
+const time = Ref{DateTime}()
+
+module FX
+
+using ..Positions
+
+const FI = FinancialInstruments
+
+struct CurrencyPair{A<:Real}
+    bid::A
+    ask::A
+end
+
+function generatepair(base::Currency{B},currency::Currency{C},bid::A,ask::A=bid) where {B,C,A<:Real}
+    pair = Symbol(B,C)
+    @eval FX const $(pair) = Ref{CurrencyPair}()
+    @eval $(pair)[] = CurrencyPair($bid,$ask)
+    baseccy = Position(getproperty(FI,B),1.)
+    ccy = Position(getproperty(FI,C),1.)
+    @eval begin
+        function Base.convert(::Type{$(typeof(baseccy))},x::$(typeof(ccy)))
+            rate = $(pair)[].bid
+            return $(typeof(baseccy))(x.amount/rate)
+        end
+    end
+end
+
 end
 
 end # module
