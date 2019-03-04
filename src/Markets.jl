fbd712bb228a4cbd77ea755f9f90d505c8002e0c
Eric Forgy <Eric.Forgy@coherentasia.com>
2019-03-05 00:38:05 +0800

    Move -> fx.jl

--- src/Markets.jl
+++ src/Markets.jl
@@ -6,35 +6,6 @@ export FX
 
 const time = Ref{DateTime}()
 
-module FX
-
-using ..Positions
-
-const FI = FinancialInstruments
-
-struct CurrencyPair{A<:Real,update}
-    bid::A
-    ask::A
-end
-CurrencyPair(bid,ask) = CurrencyPair{typeof(bid),identity}(bid,ask)
-
-update(c::CurrencyPair{A,F}) where {A,F} = F(c)
-
-function generatepair(base::Currency{B},currency::Currency{C},bid::A,ask::A=bid) where {B,C,A<:Real}
-    pair = Symbol(B,C)
-    @eval FX const $(pair) = Ref{CurrencyPair}()
-    @eval FX $(pair)[] = CurrencyPair($bid,$ask)
-    baseccy = Position(getproperty(FI,B),1.)
-    ccy = Position(getproperty(FI,C),1.)
-    @eval FX begin
-        function convert(::Type{$(typeof(baseccy))},x::$(typeof(ccy)))
-            rate = $(pair)[].bid
-            return $(typeof(baseccy))(x.amount/rate)
-        end
-    end
-    return nothing
-end
-
-end
+include("fx.jl")
 
 end # module
